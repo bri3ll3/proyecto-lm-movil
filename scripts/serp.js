70f2c3b2b3279fb8029e7f957d05e31e353380df
bri3ll3 <biacra2@alu.edu.gva.es>
2026-04-30 17:42:55 +0200

    feat: serp.js

--- scripts/serp.js
+++ scripts/serp.js
@@ -3,6 +3,10 @@ function iniciar()
 {
     canvas=document.getElementById('lienzo');
     lienzo=canvas.getContext('2d');
+    
+var gradiente=lienzo.createLinearGradient(0,0,10,100);
+    gradiente.addColorStop(0.5, '#0000FF');
+    gradiente.addColorStop(1, '#000000');
+    lienzo.fillStyle=gradiente;
+    lienzo.fillRect(10,10,100,100);
 }
-
-window.addEventListener("load", iniciar, false);
